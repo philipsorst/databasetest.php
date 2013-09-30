8377af6a3d13e20740ca6656ae800f4810f85070
Philip Washington Sorst <philip@sorst.net>
2013-09-30 12:17:01 +0200

    Adding test bootstrap

--- test/net/dontdrinkandroot/repository/RepositoryTest.php
+++ test/net/dontdrinkandroot/repository/RepositoryTest.php
@@ -2,23 +2,15 @@
 
 namespace net\dontdrinkandroot\database\repository;
 
-require_once "../../../../src/net/dontdrinkandroot/database/Database.php";
-require_once "../../../../src/net/dontdrinkandroot/database/MySqlDatabase.php";
-require_once "../../../../src/net/dontdrinkandroot/database/DatabaseManager.php";
-require_once "../../../../src/net/dontdrinkandroot/database/DatabaseFactory.php";
-require_once "../../../../src/net/dontdrinkandroot/database/DatabaseConfig.php";
-require_once "../../../../src/net/dontdrinkandroot/database/MySqlDatabaseConfig.php";
-require_once "../../../../src/net/dontdrinkandroot/repository/Repository.php";
-
 use net\dontdrinkandroot\database\MySqlDatabaseConfig;
 use net\dontdrinkandroot\database\DatabaseManager;
 
-class RepositoryTest extends \PHPUnit_Framework_TestCase {
+class RepositoryTest extends \PHPUnit_Extensions_Database_TestCase {
 
     public function testFind() {
 
         $databaseManager = new DatabaseManager();
-        $databaseConfig = new MySqlDatabaseConfig("localhost", 3306, "test", "test", "test");
+        $databaseConfig = new MySqlDatabaseConfig( "localhost", 3306, "test", "test", "test" );
         $databaseManager->registerDatabase("test", $databaseConfig);
 
         $db = $databaseManager->getDatabase("test");
